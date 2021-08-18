c8efbd0ad1d418cf642535fb50effb69b5b9cb4e
miho.toyama <miho.toyama@litalico.co.jp>
2021-08-18 16:57:22 +0900

    TypeORM+SQLiteとNestJSの接続の初期設定を行った

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PetModule } from './pet/pet.module';
 
 @Module({
-  imports: [PetModule],
+  imports: [TypeOrmModule.forRoot(), PetModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
