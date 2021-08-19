d5c1735baa9c642458b682b142fccaf3c371db75
miho.toyama <miho.toyama@litalico.co.jp>
2021-08-19 14:29:05 +0900

    petエンティティを使えるように moduleとserviceを更新

--- src/pet/pet.module.ts
+++ src/pet/pet.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PetController } from './pet.controller';
 import { PetService } from './pet.service';
+import { Pet } from '../entities/pet.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Pet])],
   controllers: [PetController],
-  providers: [PetService]
+  providers: [PetService],
 })
 export class PetModule {}
--- src/pet/pet.service.ts
+++ src/pet/pet.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Pet } from '../entities/pet.entity';
 
 @Injectable()
-export class PetService {}
+export class PetService {
+  constructor(
+    @InjectRepository(Pet)
+    private petsRepository: Repository<Pet>,
+  ) {}
+
+  findAll(): Promise<Pet[]> {
+    return this.petsRepository.find();
+  }
+
+  findOne(id: string): Promise<Pet> {
+    return this.petsRepository.findOne(id);
+  }
+
+  async remove(id: string): Promise<void> {
+    await this.petsRepository.delete(id);
+  }
+}
