4f6d9f37bde42db245fb33979651f4d96d6504f3
miho.toyama <miho.toyama@litalico.co.jp>
2021-08-24 15:38:29 +0900

    ownerエンティティの追加と、それに伴うpetエンティティの修正

---
+++ src/entities/owner.entity.ts
@@ -0,0 +1,18 @@
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { Pet } from './pet.entity';
+
+@Entity()
+export class Owner {
+  @PrimaryGeneratedColumn('uuid')
+  readonly id: number;
+
+  @Column({ length: 100 })
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  @OneToMany(() => Pet, (pet) => pet.owner)
+  pets: Pet[];
+}
--- src/entities/pet.entity.ts
+++ src/entities/pet.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { Owner } from './owner.entity';
 
 @Entity()
 export class Pet {
@@ -9,14 +10,13 @@ export class Pet {
   name: string;
 
   @Column({ length: 100 })
-  ownerName: string;
-
-  @Column({ length: 100 })
   breed: string;
 
-  constructor(name: string, ownerName: string, breed: string) {
+  constructor(name: string, breed: string) {
     this.name = name;
-    this.ownerName = ownerName;
     this.breed = breed;
   }
+
+  @ManyToOne(() => Owner, (owner) => owner.pets)
+  owner: Owner;
 }
