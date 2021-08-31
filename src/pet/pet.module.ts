9075a841226c55ccdcb2c3184156d221abb61baa
miho.toyama <miho.toyama@litalico.co.jp>
2021-08-31 11:40:14 +0900

    petモジュールに、ownerの依存関係を追記

--- src/pet/pet.module.ts
+++ src/pet/pet.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PetController } from './pet.controller';
 import { PetService } from './pet.service';
 import { Pet } from '../entities/pet.entity';
+import { OwnerService } from 'src/owner/owner.service';
+import { Owner } from 'src/entities/owner.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Pet])],
+  imports: [TypeOrmModule.forFeature([Pet, Owner])],
   controllers: [PetController],
-  providers: [PetService],
+  providers: [PetService, OwnerService],
 })
 export class PetModule {}
