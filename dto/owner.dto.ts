f26046c0f9529cef5d3fa5a376e7962d82cf6034
miho.toyama <miho.toyama@litalico.co.jp>
2021-09-03 17:31:05 +0900

    DTOにバリデーション追加

--- dto/owner.dto.ts
+++ dto/owner.dto.ts
@@ -1,7 +1,12 @@
+import { IsNotEmpty, IsString } from 'class-validator';
 import { PetDto } from './pet.dto';
 
 export class OwnerDto {
   id: string;
+
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
   pets: PetDto[];
 }
--- dto/pet.dto.ts
+++ dto/pet.dto.ts
@@ -1,8 +1,17 @@
+import { IsNotEmpty, IsString } from 'class-validator';
 import { OwnerDto } from './owner.dto';
 
 export class PetDto {
   id: string;
+
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsString()
   breed: string;
+
+  @IsNotEmpty()
   owner: OwnerDto;
 }
