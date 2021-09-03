71ff552db61a2019717e7681236d6603c37fba8b
miho.toyama <miho.toyama@litalico.co.jp>
2021-09-03 17:30:50 +0900

    Pipe使用できるようにmain.tsを変更

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -11,6 +12,8 @@ async function bootstrap() {
   app.setViewEngine('hbs');
 
   app.enableCors(); // enable CORS
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
 }
